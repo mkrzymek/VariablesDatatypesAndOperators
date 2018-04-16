7ccbd3303989091b060a63a126ed2346d821a47c
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-16 16:08:20 +0200

    float and double

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,14 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // width 32 (4 bytes)
+        int myIntValue = 5 / 3;
+        // width 32 (4 bytes)
+        float myFloatValue = 5f / 3f;
+        // width 64 (8 bytes)
+        double myDoubleValue = 5d / 3d;
+        System.out.println("myIntValue = " +myIntValue);
+        System.out.println("myFloatValue = " +myFloatValue);
+        System.out.println("myDoubleValue = " +myDoubleValue);
     }
 }
