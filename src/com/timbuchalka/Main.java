e4b6f0e5440d1e3ff66a175aacc5fb8926b44408
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 10:52:04 +0200

    Operators and operators precedence

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,9 +3,10 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-        floatAndDouble();
-        charAndBoolean();
-        primitiveTypesAndString();
+//        floatAndDouble();
+//        charAndBoolean();
+//        primitiveTypesAndString();
+        operatorsInJava();
     }
 
     private static void floatAndDouble() {
@@ -15,12 +16,12 @@ public class Main {
         float myFloatValue = 5f / 3f;
         // width 64 (8 bytes)
         double myDoubleValue = 5d / 2d;
-        System.out.println("myIntValue = " +myIntValue);
-        System.out.println("myFloatValue = " +myFloatValue);
-        System.out.println("myDoubleValue = " +myDoubleValue);
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
     }
 
-    private static void charAndBoolean () {
+    private static void charAndBoolean() {
         char myChar = 'D'; // any ONE character
         char myUnicodeChar = '\u00A9';
         System.out.println(myUnicodeChar);
@@ -28,7 +29,7 @@ public class Main {
         boolean myBoolean = true; // only true or false
     }
 
-    private static void primitiveTypesAndString () {
+    private static void primitiveTypesAndString() {
         // primitive datatypes (8)
         // byte
         // short
@@ -59,5 +60,63 @@ public class Main {
         System.out.println(lastString);
     }
 
+    private static void operatorsInJava() {
+        int result = 1 + 2;
 
+        int previousResult = result;
+
+        result = result - 1;
+        System.out.println("result = " + result);
+
+        previousResult = result;
+        result = result * 10;
+        System.out.println(previousResult + " * 10 = " + result);
+
+        previousResult = result;
+        result = result % 3;
+        System.out.println(result);
+
+        previousResult = result;
+        result++;
+        System.out.println(result);
+
+        result *= 5;
+        System.out.println(result);
+
+        result -= 10;
+        System.out.println(result);
+
+        result /= 5;
+        System.out.println(result);
+
+        boolean isAlien = false;
+        if(isAlien == true) {
+            System.out.println("It is not an Alien");
+        }
+
+        int topScore = 80;
+        if(topScore >= 100) {
+            System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 60;
+        if((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("greater than second top score and less then 100");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("one of these test is true");
+        }
+
+        int newValue = 50;
+        if(newValue == 50)
+            System.out.println("this is an error");
+
+        boolean isCar = false;
+        if (isCar = true) {
+            System.out.println("to nie powinno sie zdarzyc");
+        }
+
+        boolean wasCar = isCar ? true : false;
+    }
 }
